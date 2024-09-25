77f50a28b06d3765a7ee88ee9022bf65c9dcc78c
Andrei Brizhinev <bragovo@gmail.com>
2024-09-25 18:51:15 +0300

    Add complete endpoint

--- src/app/auth.rs
+++ src/app/auth.rs
@@ -1,5 +1,5 @@
 use axum::{extract::State, routing::post, Json, Router};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 use super::{error::AppError, state::AppState};
@@ -8,7 +8,7 @@ pub fn router() -> Router<AppState> {
     Router::new()
         // .route("/login", post(controller::login))
         .route("/join", post(join))
-    // .route("/complete", post(controller::complete))
+        .route("/complete", post(complete))
     // .route("/me", get(controller::me))
 }
 
@@ -20,11 +20,14 @@ async fn join(
     Json(data): Json<JoinRequest>,
 ) -> Result<Json<Value>, AppError> {
     let request: flux_auth_api::JoinRequest = data.into();
-    let response = auth_service_client.clone().join(request).await?.into_inner();
+    let response = auth_service_client
+        .clone()
+        .join(request)
+        .await?
+        .into_inner();
 
     Ok(Json(serde_json::from_str(response.response())?))
 }
-
 
 #[derive(Deserialize)]
 struct JoinRequest {
@@ -33,6 +36,40 @@ struct JoinRequest {
 
 impl From<JoinRequest> for flux_auth_api::JoinRequest {
     fn from(request: JoinRequest) -> Self {
-        Self { email: request.email }
+        Self {
+            email: request.email,
+        }
     }
+}
+
+async fn complete(
+    State(AppState {
+        auth_service_client,
+        ..
+    }): State<AppState>,
+    Json(data): Json<Value>,
+) -> Result<Json<CompleteResponse>, AppError> {
+    let request = flux_auth_api::CompleteRequest {
+        request: Some(data.to_string()),
+    };
+    let response = auth_service_client
+        .clone()
+        .complete(request)
+        .await?
+        .into_inner();
+
+    Ok(Json(response.into()))
+}
+
+impl Into<CompleteResponse> for flux_auth_api::CompleteResponse {
+    fn into(self) -> CompleteResponse {
+        CompleteResponse {
+            jwt: self.jwt().into(),
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct CompleteResponse {
+    pub jwt: String,
 }
