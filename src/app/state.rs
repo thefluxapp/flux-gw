65c7209a1d4650ec813f5b4920b96de42f128cae
Andy Braga <bragovo@gmail.com>
2024-10-20 14:35:19 +0300

    Lazy init gRPC clients (#2)

--- src/app/state.rs
+++ src/app/state.rs
@@ -3,7 +3,6 @@ use flux_auth_api::auth_service_client::AuthServiceClient;
 use flux_core_api::streams_service_client::StreamsServiceClient;
 use tokio::fs;
 use tonic::transport::Channel;
-// use flux_auth_api::auth_service_client;
 
 use super::settings::AppSettings;
 
@@ -18,10 +17,10 @@ pub struct AppState {
 impl AppState {
     pub async fn new(settings: AppSettings) -> Result<Self, Error> {
         let auth_service_client =
-            AuthServiceClient::connect(settings.clients.flux_auth.endpoint.clone()).await?;
+            Self::auth_service_client(settings.clients.flux_auth.endpoint.clone()).await?;
 
         let streams_service_client =
-            StreamsServiceClient::connect(settings.clients.flux_core.endpoint.clone()).await?;
+            Self::streams_service_client(settings.clients.flux_core.endpoint.clone()).await?;
 
         let public_key = fs::read_to_string(&settings.auth.public_key_file)
             .await?
@@ -33,5 +32,17 @@ impl AppState {
             streams_service_client,
             public_key,
         })
+    }
+
+    async fn auth_service_client(dst: String) -> Result<AuthServiceClient<Channel>, Error> {
+        let ch = tonic::transport::Endpoint::new(dst)?.connect_lazy();
+
+        Ok(AuthServiceClient::new(ch))
+    }
+
+    async fn streams_service_client(dst: String) -> Result<StreamsServiceClient<Channel>, Error> {
+        let ch = tonic::transport::Endpoint::new(dst)?.connect_lazy();
+
+        Ok(StreamsServiceClient::new(ch))
     }
 }
