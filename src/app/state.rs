a1b5cc69e9ef59d9a40e58a8cae2b1ab1926ab32
Andy Braga <bragovo@gmail.com>
2024-10-27 10:48:22 +0300

    Add messages module (#3)

--- src/app/state.rs
+++ src/app/state.rs
@@ -1,6 +1,10 @@
 use anyhow::Error;
-use flux_auth_api::auth_service_client::AuthServiceClient;
-use flux_core_api::streams_service_client::StreamsServiceClient;
+use flux_auth_api::{
+    auth_service_client::AuthServiceClient, users_service_client::UsersServiceClient,
+};
+use flux_core_api::{
+    messages_service_client::MessagesServiceClient, streams_service_client::StreamsServiceClient,
+};
 use tokio::fs;
 use tonic::transport::Channel;
 
@@ -10,7 +14,9 @@ use super::settings::AppSettings;
 pub struct AppState {
     pub settings: AppSettings,
     pub auth_service_client: AuthServiceClient<Channel>,
+    pub users_service_client: UsersServiceClient<Channel>,
     pub streams_service_client: StreamsServiceClient<Channel>,
+    pub messages_service_client: MessagesServiceClient<Channel>,
     pub public_key: Vec<u8>,
 }
 
@@ -19,8 +25,14 @@ impl AppState {
         let auth_service_client =
             Self::auth_service_client(settings.clients.flux_auth.endpoint.clone()).await?;
 
+        let users_service_client =
+            Self::users_service_client(settings.clients.flux_auth.endpoint.clone()).await?;
+
         let streams_service_client =
             Self::streams_service_client(settings.clients.flux_core.endpoint.clone()).await?;
+
+        let messages_service_client =
+            Self::messages_service_client(settings.clients.flux_core.endpoint.clone()).await?;
 
         let public_key = fs::read_to_string(&settings.auth.public_key_file)
             .await?
@@ -29,7 +41,9 @@ impl AppState {
         Ok(Self {
             settings,
             auth_service_client,
+            users_service_client,
             streams_service_client,
+            messages_service_client,
             public_key,
         })
     }
@@ -40,9 +54,21 @@ impl AppState {
         Ok(AuthServiceClient::new(ch))
     }
 
+    async fn users_service_client(dst: String) -> Result<UsersServiceClient<Channel>, Error> {
+        let ch = tonic::transport::Endpoint::new(dst)?.connect_lazy();
+
+        Ok(UsersServiceClient::new(ch))
+    }
+
     async fn streams_service_client(dst: String) -> Result<StreamsServiceClient<Channel>, Error> {
         let ch = tonic::transport::Endpoint::new(dst)?.connect_lazy();
 
         Ok(StreamsServiceClient::new(ch))
+    }
+
+    async fn messages_service_client(dst: String) -> Result<MessagesServiceClient<Channel>, Error> {
+        let ch = tonic::transport::Endpoint::new(dst)?.connect_lazy();
+
+        Ok(MessagesServiceClient::new(ch))
     }
 }
