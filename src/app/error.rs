b6d67b5b230f6b3d6feb3c023df49fc816525c37
Andrei Brizhinev <bragovo@gmail.com>
2025-05-11 14:25:11 +0300

    some error

--- src/app/error.rs
+++ src/app/error.rs
@@ -3,9 +3,12 @@ use axum::{
     response::{IntoResponse, Response},
 };
 use axum_extra::typed_header::TypedHeaderRejection;
+use tracing::error;
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
+        error!("{}", self.to_string());
+
         match self {
             AppError::Status(status) => match status.code() {
                 tonic::Code::InvalidArgument => (StatusCode::UNPROCESSABLE_ENTITY, "".to_string()),
