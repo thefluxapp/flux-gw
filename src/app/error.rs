7f4ec91e26e3221e56811e362158680948c4fbe7
Andy Braga <bragovo@gmail.com>
2025-06-01 21:16:17 +0300

    Add deploy configs (#14)
    * Add deploy configs
    
    * Add protobuf
    
    * Try dev
    
    * Rename legacy
    
    * debug
    
    * some error

--- src/app/error.rs
+++ src/app/error.rs
@@ -3,15 +3,18 @@ use axum::{
     response::{IntoResponse, Response},
 };
 use axum_extra::typed_header::TypedHeaderRejection;
+use tracing::error;
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
+        error!("{}", self.to_string());
+
         match self {
             AppError::Status(status) => match status.code() {
-                tonic::Code::InvalidArgument => StatusCode::UNPROCESSABLE_ENTITY,
-                _ => StatusCode::BAD_REQUEST,
+                tonic::Code::InvalidArgument => (StatusCode::UNPROCESSABLE_ENTITY, "".to_string()),
+                code => (StatusCode::BAD_REQUEST, code.to_string()),
             },
-            _ => StatusCode::BAD_REQUEST,
+            error => (StatusCode::BAD_REQUEST, error.to_string()),
         }
         .into_response()
     }
